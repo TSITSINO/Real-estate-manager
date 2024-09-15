61fb2b619d3b26edcf409c93575ebf19105e33ff
tsitsino <tsitsilashvilitsitsino@gmail.com>
2024-09-16 01:05:16 +0400

    create home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Region } from '../../api/model';
+import { GeographicalInformationService } from '../../api/services/geographical-information.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  geographicalInformationService = inject(GeographicalInformationService);
+  regions = false;
+  area = false;
+  priceCategory = false;
+  numberOfBedrooms = false;
+  regions$: Observable<Region[]> | undefined;
 
+  ngOnInit(): void {
+    this.regions$ = this.geographicalInformationService.getRegions();
+  }
 }
