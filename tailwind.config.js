407585eeffcf7216ee012068de15e2a1c41771e3
tsitsino <tsitsilashvilitsitsino@gmail.com>
2024-09-16 00:58:50 +0400

    set custom color and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    textColor: (theme) => theme("colors"),
+    textColor: {
+      primary: "#F93B1D",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+      white: "#FFFFFF",
+    },
+    colors: {
+      primary: "#F93B1D",
+      gray: "#DBDBDB",
+      white: "#FFFFFF",
+      dark: "#DF3014",
+    },
   },
   plugins: [],
 };
