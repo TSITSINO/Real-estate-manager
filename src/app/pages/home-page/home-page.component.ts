f2e56fc3a2ec49c44e301202a09ccdbb997a7665
tsitsino <tsitsilashvilitsitsino@gmail.com>
2024-09-17 15:18:27 +0400

    add real estate card ar home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,26 +1,31 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { Region } from '../../api/model';
+import { RealEstate, Region } from '../../api/model';
 import { GeographicalInformationService } from '../../api/services/geographical-information.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RealEstateCardComponent } from '../../components/real-estate-card/real-estate-card.component';
+import { RealEstateService } from '../../api/services/real-estates.service';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, RealEstateCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent implements OnInit {
   geographicalInformationService = inject(GeographicalInformationService);
+  realEstateService = inject(RealEstateService);
   regions = false;
   area = false;
   priceCategory = false;
   numberOfBedrooms = false;
   regions$: Observable<Region[]> | undefined;
+  realEstates$: Observable<RealEstate[]> | undefined;
 
   ngOnInit(): void {
     this.regions$ = this.geographicalInformationService.getRegions();
+    this.realEstates$ = this.realEstateService.getRealEstates();
   }
 }
